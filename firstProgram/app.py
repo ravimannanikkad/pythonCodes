7d66d1c002db2f1c995d270d192c28b33933aa02
Ravi Mannanikkad <ravimannanikkad@gmail.com>
2020-04-08 15:30:22 +0200

    dictionay updated

--- firstProgram/app.py
+++ firstProgram/app.py
@@ -1,85 +1,34 @@
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-# Writen By Ravi Damodaran #
-# Python Number Tutorial
-# Working with Number #
-=======
-character_name = "Joseph"
-character_age = "70"
->>>>>>> parent of e2ad56d... Working with Strings
-=======
-character_name = "Joseph"
-character_age = "70"
->>>>>>> parent of c7bf1af... Working with Strings
-
-from math import *
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-#1 Printing a Number
-print("1", end =": ")
-print(100)
-
-#2 Printing a Decimal Number
-print("2", end =": ")
-print(20148.258)
-
-#3 Printing a negative Number
-print("3", end =": ")
-print(-20148.258)
+#***************************#
+# Writen By Ravi Damodaran  #
+# Python Dictionary Tutorial#
+# Working with Dictionary   #
+#***************************#
 
 
-#4 Printing a number from variable
-my_num=52
-print("4", end =": ")
-print(my_num)
+monthConversions = {
+    #Key : Value
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+# Method 1 to get value from dictionary
+print("Get the value of Oct", end= ":= ")
+print(monthConversions["Oct"])
 
-#5 Converting a number to string
-print("5", end =": ")
-print(str(my_num)+ " is My fav number")
+# Method 2 to get value from dictionary
+print("Get the value of Mar", end= ":= ")
+print(monthConversions.get("Mar","Not a valid Key"))
 
-#6 Absolute value of number
-my_num2= -98
-print("6", end =": ")
-print(abs(my_num2))
-
-#7 finding power of a number ie here it will be 52^5
-print("7", end =": ")
-print(pow(my_num,5))
-
-#9 Finding the Largest number of 12547869 and 1256879
-print("8", end =": ")
-print(max(12547869,1256879))
-
-#8 Finding the smallest number of 12547869 and 1256879
-print("9", end =": ")
-print(min(12547869,1256879))
-
-#10 rounding the number 3.84
-print("10", end =": ")
-print(round(3.84))
-
-#11 chopping the decimal points
-print("11", end =": ")
-print(floor(3.85))
-
-#12 chopping the decimal points and round up to the next number
-print("12", end =": ")
-print(ceil(3.15))
-
-#13 Square root of a number
-print("13", end =": ")
-print(sqrt(36))
-=======
-=======
->>>>>>> parent of c7bf1af... Working with Strings
-print("There was a man named "+ character_name +",")
-print("he was "+ character_age + " years old.")
-print("He really likes the name  "+ character_name +",")
-print("but didn't like being " + character_age + ".")
-<<<<<<< HEAD
->>>>>>> parent of e2ad56d... Working with Strings
-=======
->>>>>>> parent of c7bf1af... Working with Strings
+# Method 2 to get value from dictionary when there is not that value is present
+print("Get the value of Ravi", end= ":= ")
+print(monthConversions.get("Ravi","Not a valid Key"))
