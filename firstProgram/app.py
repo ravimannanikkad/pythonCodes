7962476fd76a9b9064b0faf6f00c0ed7760aa3ce
Ravi Mannanikkad <ravimannanikkad@gmail.com>
2020-04-08 10:20:58 +0200

    Variables introduction

--- firstProgram/app.py
+++ firstProgram/app.py
@@ -1,5 +1,9 @@
-print("Hello World")
-print("    /|")
-print("   / | ")
-print("  /  |")
-print(" /___|")
+
+character_name = "Joseph"
+character_age = "70"
+
+
+print("There was a man named "+ character_name +",")
+print("he was "+ character_age + " years old.")
+print("He really likes the name  "+ character_name +",")
+print("but didn't like being " + character_age + ".")
