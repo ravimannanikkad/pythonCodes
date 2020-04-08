cd647d8747e28d8d9e18976825f15baeba9170e5
Ravi Mannanikkad <ravimannanikkad@gmail.com>
2020-04-08 22:20:13 +0200

    finished 2d list

--- firstProgram/app.py
+++ firstProgram/app.py
@@ -1,17 +1,47 @@
 
 #************************************#
 # Writen By Ravi Damodaran           #
-# Python exponent function Tutorial  #
-# Working with exponent function     #
+# Python 2D list and nested for loop #
+# Working with 2D list and nested for loop#
 #************************************#
 
-
-# A function to find the exponential of the base number
-def rasie_to_power(base_num,power_num):
-    result =1
-    for index in range(power_num):
-        result=result*base_num
-    return result
-
-print(rasie_to_power(4,3))
-
+# 2 Dimensional Matrix
+number_grid =[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [0]
+]
+#print 2D array using nested for loop
+for row in number_grid:
+    for col in row:
+        print(col)
+# 3 Dimensional Matrix
+number_grid2=[
+    [[1,2,3],
+     [4,5,6],
+     [7,8,9]],
+    [[11,12,13],
+     [14,15,16],
+     [17,18,19]],
+    [[21,22,23],
+     [24,25,26],
+     [27,28,29]]
+]
+#print ED array using nested for loop
+for segment in number_grid2:
+    for row in segment:
+        for col in row:
+            print(col)
+# 3 Dimensional Matrix
+number_grid2=[
+    [[1,2,3],
+     [4,5,6],
+     [7,8,9]],
+    [[11,12,13],
+     [14,15,16],
+     [17,18,19]],
+    [[21,22,23],
+     [24,25,26],
+     [27,28,29]]
+]
