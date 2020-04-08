9844556c7e612e2be15333b2849c9cbc87824817
Ravi Mannanikkad <ravimannanikkad@gmail.com>
2020-04-08 17:02:51 +0200

    Finished the for loop

--- firstProgram/app.py
+++ firstProgram/app.py
@@ -1,46 +1,25 @@
 
 #***************************#
 # Writen By Ravi Damodaran  #
-# Python while loop Tutorial#
-# Working with while loop   #
+# Python for loop Tutorial  #
+# Working with for loop     #
 #***************************#
 
-# i =1
-#
-# while i<=10:
-#     print(i)
-#     i+=1
-#
-# print("Done with loop")
+# print the whole letters in the string Ravi Mannanikkad
+for letter in "Ravi Mannanikkad":
+    print(letter)
 
-secret_word="ravi"
-guess=""
-guess_count=0
-guess_limit=3
-out_of_guesses=False
-while secret_word !=guess and not(out_of_guesses):
-    if guess_count<guess_limit:
-        guess=input("Enter your guess: ")
-        guess_count+=1
-    else:
-        out_of_guesses=True
+# A list of friends
+friends= ["Ammu","Appu","Amrita","Mahesh","Ravi"]
+#Print all the friends in the list using for loop
+for f in friends:
+    print(f)
+#print integers from 2 to 30
+for i in range (2,30):
+    print(i)
 
-    if guess != secret_word:
-        print("Sorry Wrong Guess")
-if out_of_guesses:
-    print("You lost the game")
-else:
-    print("you win !")
-
-
-
-
-
-
-
-
-
-
+for i in range(len(friends)):
+    print(friends[i])
 
 
 
