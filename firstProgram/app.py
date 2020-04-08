be83070f6c725cef334010774d9ea86d8dca2291
Ravi Mannanikkad <ravimannanikkad@gmail.com>
2020-04-08 17:12:12 +0200

    Finished the exponentfunction

--- firstProgram/app.py
+++ firstProgram/app.py
@@ -1,25 +1,17 @@
 
-#***************************#
-# Writen By Ravi Damodaran  #
-# Python for loop Tutorial  #
-# Working with for loop     #
-#***************************#
-
-# print the whole letters in the string Ravi Mannanikkad
-for letter in "Ravi Mannanikkad":
-    print(letter)
-
-# A list of friends
-friends= ["Ammu","Appu","Amrita","Mahesh","Ravi"]
-#Print all the friends in the list using for loop
-for f in friends:
-    print(f)
-#print integers from 2 to 30
-for i in range (2,30):
-    print(i)
-
-for i in range(len(friends)):
-    print(friends[i])
+#************************************#
+# Writen By Ravi Damodaran           #
+# Python exponent function Tutorial  #
+# Working with exponent function     #
+#************************************#
 
 
+# A function to find the exponential of the base number
+def rasie_to_power(base_num,power_num):
+    result =1
+    for index in range(power_num):
+        result=result*base_num
+    return result
+
+print(rasie_to_power(4,3))
 
