2d6640b99f8f05e6114672ec3604924381e5e530
Ravi Mannanikkad <ravimannanikkad@gmail.com>
2020-04-08 12:38:03 +0200

    listFunctins examples

--- firstProgram/app.py
+++ firstProgram/app.py
@@ -4,29 +4,56 @@
 # Working with Lists #
 
 #creating a list of friends here
-friends =["ammu","Appu","Ramu","June","Mahesh","Ravi","Aravind"]
+friends =["Ammu","Appu","Ramu","June","Ammu","Mahesh","Ravi","Aravind"]
+#creating a list of lucky numbers here
+lucky_numbers=[4,58,5,9,6,7,3,78]
 
-# printing a list here
-print("List of friends",end=": ")
+# print the firends list and lucky numbers
+print("Print the list friends: ", end= ": ")
+print(friends)
+print("Print the list lucky numbers: ", end= ": ")
+print(lucky_numbers)
+
+# Adding an element to the end of the friends list
+print("Append the name \"Aparna\" to the end of friends list", end= ": ")
+friends.append("Aparna")
 print(friends)
 
-#printing the contents of list using index
-print("The friend on index 0",end=": ")
-print(friends[0])
+# Adding an element to the middle of the friends list
 
-#printing the contents of list using negative index
-print("The friend on index -1",end=": ")
-print(friends[-1])
+print("Add the name \"Amrita\" to the index 4", end= ": ")
+friends.insert(4,"Amrita")
+print(friends)
 
-#printing one part of the contents of list
-print("The part of contents in the list from index 1",end=": ")
-print(friends[1:])
+# Removing an element from the list
+print("Remove the name \"June\" from the list", end= ": ")
+friends.remove("June")
+print(friends)
 
-#printing one portion of the contents of list
-print("The portion of contents in the list from index 1 to 4",end=": ")
-print(friends[1:4])
+# Finding the index of an item in the list if it exists
+print("Finding the index of name \"Amrita\" from the list", end= ": ")
+print(friends.index("Amrita"))
 
-#Modifying values inside list
-print("Modify value of Appu to Raman")
-friends[1]="Raman"
+# Counting the number of times the same element exist
+print("NUmber of \"Ammu\" in the list", end= ": " )
+print(friends.count("Ammu"))
+
+# Sorting the list in ascending order
+print("Sorting the friends list in ascending order", end= ": " )
+friends.sort()
+print(friends)
+
+# Sorting the list in ascending order
+print("Sorting the lucky numbers list in ascending order", end= ": " )
+lucky_numbers.sort()
+print(lucky_numbers)
+
+# Appending the friends list with lucky numbers
+print("The extended/ Appended list of of friends with lucky numbers: ", end= ": ")
+friends.extend(lucky_numbers)
+print(friends)
+
+# Removing all the values from the list
+print("Removing all the values from list", end= ": ")
+friends.clear()
 print(friends)
