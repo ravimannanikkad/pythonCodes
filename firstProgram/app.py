6338eab5e59978d7fb41e6605e5f009dfa0f18c5
Ravi Mannanikkad <ravimannanikkad@gmail.com>
2020-04-08 13:15:42 +0200

    Updated the functions

--- firstProgram/app.py
+++ firstProgram/app.py
@@ -1,15 +1,31 @@
-
 # Writen By Ravi Damodaran #
-# Python Tuples Tutorial
-# Working with Tuples #
+# Python Functions Tutorial
+# Working with Functions #
 
-# 1. Couples are immutable, that means it cant be changed once its created
+# Simple Function
+def say_hi():
+    print("Hello User")
 
-#make a tuple with elementaes
-coordinates=(4,5,8)
-#print the tuple
-print("The value of Tuple in coordinates are ", end=": ")
-print(coordinates)
-#print one lement of tuple
-print("The value of index 2 in Tuple of coordinates are ", end=": ")
-print(coordinates[2])
+
+# Function with input Parameters
+def say_me(name, age):
+    print("Hello " + name + ", You are "+ age+ " years old")
+
+def calculate_multiples(num):
+    res1=num*num
+    res2=res1*num
+    #returning 2 results
+    return res1 ,res2
+
+
+# Calling a simple function
+say_hi()
+# Calling a function with input parameters
+say_me("Ravi","31")
+# Calling a function with input parameters and receiving value
+print("The square of the number 4 is ",end= ": ")
+#getting results to two variables
+result1,result2 =calculate_multiples(4)
+print(result1)
+print("The cube of the number 4 is ",end= ": ")
+print(result2)
