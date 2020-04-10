72f20ba169eab1ecb6b7186d22a03b24b7531014
Ravi Mannanikkad <ravimannanikkad@gmail.com>
2020-04-10 23:51:54 +0200

    Finished the linked list example with get_contents and Count_nodes function

---
+++ dataStructures/venv/Linklists.py
@@ -0,0 +1,9 @@
+
+# A class created for Nodes
+class Node:
+    # Constructor with attributes data
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
--- dataStructures/venv/app.py
+++ dataStructures/venv/app.py
@@ -1,27 +1,65 @@
-#****************************************************#
+# ****************************************************#
 # Created By: Ravi Damodaran
 # Organization: Vaayoo
-# Classes and Inheritance
-#****************************************************#
+# Link lists
+# ****************************************************#
 
-# Import the classes from the file robot.py
-from robot import Robot,RaviRobot
+# Import the class Node from the file Linklists.py
+from Linklists import Node
 
-# create object for 1st Robot
-r1 = Robot("Ravi","Red",30)
-# create object for 2nd Robot
-r2 = Robot("Amrita","Yellow",60)
-# create object for 3rd Robot using class RaviRobot
-r3 = RaviRobot("India")
-# Set the inheriting parameters of the class
-# RaviRobot using the following
-r3.name="Appy"
-r3.color="June"
-r3.weight=90
 
-#Call the functions in Robot class
-r1.introduce_self()
-r2.introduce_self()
-#Call the functions in RaviRobot class
-r3.introduce_self()
-r3.location_now()
+############ Functions in the program ####################
+# A funtion to count the total number
+# of nodes in the link lists
+def count_nodes(head):
+    # assuming that head != None
+    count = 1                                           # counter for counting the number of nodes. Initial value is one
+    current = head                                      # Implying the head object to variable current
+    while current.next is not None:                     # A while loop loops while the node.next is not none
+        current = current.next                          # Changing the current node to next linked node
+        count += 1                                      # Incrementing the counter
+    return count                                        # Returning the counter at the end of the loop
+
+
+# A function to get the data from each node
+def get_contents(node):
+    return node.data                                    # Returns the data of the node from the node object
+
+
+########## Start of Program ##############################
+
+
+nodes = [Node(6),
+         Node(3),
+         Node(4),
+         Node(2),
+         Node(1)]
+
+# A loop to connect the node links
+index=0
+for i in nodes:                                     # Iterating through the nodes dictionary, where i is the key value
+    # print(i)
+    try:                                            # There is chances to get error hence try and except used
+        if index <(len(nodes)-1):                   # An if to compare the index is less than the length to avoid error in index addition
+            nodes[index].next = nodes[index+1]      # Assigning the "next" variable in the object as the next object in dictionary
+        if index >0:                                # An if to  compare the index is greater than 0 to avoid error in index subtraction
+            nodes[index].prev = nodes[index - 1]    # Assigning the "prev" variable in the object as the previous object in dictionary
+        index+=1
+    except IndexError:
+        pass                                        # Even if there is an error pass that and move to next step
+        # print("An error occured")
+
+# Print the length of the linked list
+print("The linked list length is: " + str(count_nodes(nodes[1])))
+
+# A for loop to print the data stored in the liked list
+for node in nodes:
+    print(get_contents(node))
+# A for loop to print the contents and values of the attributes of the linked list
+for node in nodes:
+    print("Printing the values of Nodes\n" +
+          "Data = " + str(node.data) + "\n" +
+          "Next = " + str(node.next) + "\n" +
+          "Previous = " + str(node.prev) + "\n"
+          )
+    # print(nodes[node])
